425d55c237d11f4a6eb008f7d7fc73ad8cf22374
Colin Millar <colinpmillar@gmail.com>
2024-09-30 13:23:18 +0200

    start clean of data scripts

---
+++ data.R
@@ -0,0 +1,10 @@
+## Preprocess data, write TAF data tables
+
+## Before:
+## After:
+
+library(icesTAF)
+
+mkdir("data")
+
+source.taf("data_preprocess.R")
--- data_process.R
+++ data_process.R
@@ -10,95 +10,95 @@ library(sf)
 
 grid_out <- NULL
 
-for(i in 2018:2023){
+for (i in 2018:2023) {
+  vms <- read.csv(paste0("data/NEAFC_TACSAT_", i, ".csv"))
 
-vms <- read.csv(paste0("data/NEAFC_TACSAT_", i, ".csv"))
+  vms <- vms %>%
+    filter(SI_SP >= 1 & SI_SP <= 6)
 
-vms <- vms %>%
-  filter(SI_SP>= 1 & SI_SP <=6)
-
-catch <- read.csv(paste0("data/NEAFC_EFLALO_", i, ".csv"))
-
-
-# Helper function to convert date and time strings to POSIXct
-convert_datetime <- function(date, time) {
-  paste(date, time) %>% dmy_hm()
-}
-
-# Process VMS data
-vms <- vms %>%
-  mutate(datetime = convert_datetime(SI_DATE, SI_TIME))
-
-# Process catch data
-catch <- catch %>%
-  mutate(
-    trip_start = convert_datetime(FT_DDAT, FT_DTIME),
-    trip_end = convert_datetime(FT_LDAT, FT_LTIME)
-  )
-
-# Extract species names
-species_cols <- grep("^LE_KG_", names(catch), value = TRUE)
-species_names <- sub("^LE_KG_", "", species_cols)
-
-# Reshape catch data from wide to long format
-catch_long <- catch %>%
-  pivot_longer(cols = all_of(species_cols),
-               names_to = "species",
-               values_to = "catch_kg") %>%
-  mutate(species = sub("^LE_KG_", "", species)) %>%
-  filter(!is.na(catch_kg) & catch_kg > 0)
+  catch <- read.csv(paste0("data/NEAFC_EFLALO_", i, ".csv"))
 
 
-# Process VMS data
-vms_processed <- vms %>%
-  mutate(
-    date = as.Date(SI_DATE, format = "%d/%m/%Y"),
-    trip_day_id = paste(VE_REF, format(date, "%Y%m%d"), sep = "_")
-  )
+  # Helper function to convert date and time strings to POSIXct
+  convert_datetime <- function(date, time) {
+    paste(date, time) %>% dmy_hm()
+  }
 
-# Process catch data
-catch_processed <- catch_long %>%
-  mutate(
-    trip_start = as.Date(FT_DDAT, format = "%d/%m/%Y"),
-    trip_end = as.Date(FT_LDAT, format = "%d/%m/%Y"),
-    trip_length = as.numeric(trip_end - trip_start) + 1
-  ) %>%
-  filter(!is.na(trip_start) & !is.na(trip_end)) %>%
-  rowwise() %>%
-  mutate(
-    trip_days = list(seq(trip_start, trip_end, by = "day"))
-  ) %>%
-  unnest(trip_days) %>%
-  mutate(
-    trip_day_id = paste(VE_REF, format(trip_days, "%Y%m%d"), sep = "_")
-  ) %>%
-  group_by(trip_day_id, species) %>%
-  summarise(total_catch = sum(catch_kg, na.rm = TRUE), .groups = "drop")
+  # Process VMS data
+  vms <- vms %>%
+    mutate(datetime = convert_datetime(SI_DATE, SI_TIME))
 
-# Merge VMS and catch data
-merged_data <- vms_processed %>%
-  left_join(catch_processed, by = "trip_day_id", relationship = "many-to-many") %>%
-  filter(!is.na(total_catch)) %>%  # Remove VMS points not associated with a catch
-  group_by(trip_day_id, species) %>%
-  mutate(
-    points_per_day = n(),
-    distributed_catch = total_catch / points_per_day
-  ) %>%
-  ungroup()
+  # Process catch data
+  catch <- catch %>%
+    mutate(
+      trip_start = convert_datetime(FT_DDAT, FT_DTIME),
+      trip_end = convert_datetime(FT_LDAT, FT_LTIME)
+    )
 
-# Create 0.05 x 0.05 degree grid
-grid_data <- merged_data %>%
-  mutate(
-    lat_bin = floor(SI_LATI / 0.05) * 0.05,
-    lon_bin = floor(SI_LONG / 0.05) * 0.05
-  ) %>%
-  group_by(lat_bin, lon_bin, species) %>%
-  summarise(total_catch = sum(distributed_catch, na.rm = TRUE), .groups = "drop")
+  # Extract species names
+  species_cols <- grep("^LE_KG_", names(catch), value = TRUE)
+  species_names <- sub("^LE_KG_", "", species_cols)
 
- grid_data$year <- i
+  # Reshape catch data from wide to long format
+  catch_long <- catch %>%
+    pivot_longer(
+      cols = all_of(species_cols),
+      names_to = "species",
+      values_to = "catch_kg"
+    ) %>%
+    mutate(species = sub("^LE_KG_", "", species)) %>%
+    filter(!is.na(catch_kg) & catch_kg > 0)
 
- grid_out <- rbind(grid_out, grid_data)
- 
+
+  # Process VMS data
+  vms_processed <- vms %>%
+    mutate(
+      date = as.Date(SI_DATE, format = "%d/%m/%Y"),
+      trip_day_id = paste(VE_REF, format(date, "%Y%m%d"), sep = "_")
+    )
+
+  # Process catch data
+  catch_processed <- catch_long %>%
+    mutate(
+      trip_start = as.Date(FT_DDAT, format = "%d/%m/%Y"),
+      trip_end = as.Date(FT_LDAT, format = "%d/%m/%Y"),
+      trip_length = as.numeric(trip_end - trip_start) + 1
+    ) %>%
+    filter(!is.na(trip_start) & !is.na(trip_end)) %>%
+    rowwise() %>%
+    mutate(
+      trip_days = list(seq(trip_start, trip_end, by = "day"))
+    ) %>%
+    unnest(trip_days) %>%
+    mutate(
+      trip_day_id = paste(VE_REF, format(trip_days, "%Y%m%d"), sep = "_")
+    ) %>%
+    group_by(trip_day_id, species) %>%
+    summarise(total_catch = sum(catch_kg, na.rm = TRUE), .groups = "drop")
+
+  # Merge VMS and catch data
+  merged_data <- vms_processed %>%
+    left_join(catch_processed, by = "trip_day_id", relationship = "many-to-many") %>%
+    filter(!is.na(total_catch)) %>% # Remove VMS points not associated with a catch
+    group_by(trip_day_id, species) %>%
+    mutate(
+      points_per_day = n(),
+      distributed_catch = total_catch / points_per_day
+    ) %>%
+    ungroup()
+
+  # Create 0.05 x 0.05 degree grid
+  grid_data <- merged_data %>%
+    mutate(
+      lat_bin = floor(SI_LATI / 0.05) * 0.05,
+      lon_bin = floor(SI_LONG / 0.05) * 0.05
+    ) %>%
+    group_by(lat_bin, lon_bin, species) %>%
+    summarise(total_catch = sum(distributed_catch, na.rm = TRUE), .groups = "drop")
+
+  grid_data$year <- i
+
+  grid_out <- rbind(grid_out, grid_data)
 }
 
 grid_data <- grid_out
@@ -118,4 +118,3 @@ grid_data <- filtered_grid_data %>%
   st_drop_geometry() %>%
   bind_cols(st_coordinates(filtered_grid_data) %>% as_tibble()) %>%
   rename(lon_bin = X, lat_bin = Y)
-
